adcd649fd736e0467947708ebf60241fb9d8ab18
Ganesh <ganeshgavhad9@gmail.com>
2021-07-14 12:10:43 +0530

    Refactored : initial commit

--- FindMaxUsingGeneric.java
+++ FindMaxUsingGeneric.java
@@ -2,24 +2,8 @@ package com.brigdelabz;
 
 public class FindMaxUsingGeneric {
 
-	public int findMaxInteger(Integer num1, Integer num2, Integer num3) {
-		int max = num1;
-		if (num1.compareTo(max) > 0) {
-			max = num1;
-		}
-		if (num2.compareTo(max) > 0) {
-			max = num2;
-		}
-		if (num3.compareTo(max) > 0) {
-			max = num3;
-		}
-		return max;
-	}
-
 	public static void main(String[] args) {
-		FindMaxUsingGeneric obj = new FindMaxUsingGeneric();
-		int max = obj.findMaxInteger(12, 30, 25);
-		System.out.println(max);
 
+		System.out.println("Solving Problem byusing Generic");
 	}
 }
